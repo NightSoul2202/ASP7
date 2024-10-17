8cc440902b9f6ace20c8adc44e3a66d6363f5ed9
NightSoul2202 <95627778+NightSoul2202@users.noreply.github.com>
2024-10-17 11:11:51 +0300

    Initial commit

---
+++ ASP7/Controllers/FileController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text;
+
+namespace ASP7.Controllers
+{
+    public class FileController : Controller
+    {
+
+        public FileController() { }
+
+        
+        public IActionResult DownloadFile() 
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult DownloadFile(string firstName, string lastName, string fileName)
+        {
+            if (string.IsNullOrEmpty(fileName))
+            {
+                fileName = "defaultFileName";
+            }
+
+            string fileContent = $"Ім'я: {firstName}\nПрізвище: {lastName}";
+            byte[] byteArray = Encoding.UTF8.GetBytes(fileContent);
+
+            return File(byteArray, "text/plain", $"{fileName}.txt");
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
